053ed10abafaaea32a75df40e7ea07161bcedc84
Sean Bigelow <sean.bigelow1@gmail.com>
2020-12-07 17:47:20 +0000

    Automatically backed up by Learn

---
+++ lib/genre.rb
@@ -0,0 +1,41 @@
+class Genre
+
+  extend Concerns::Findable
+
+  attr_accessor :name
+  attr_reader :songs
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+  
+  def artists
+    songs.collect {|key| key.artist}.uniq
+  end
+  
+  def songs
+    @songs
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.destroy_all
+    all.clear
+  end
+  
+  def save
+    self.class.all << self
+  end
+  
+  def self.create(name)
+    genres = new(name)
+    genres.save
+    genres
+  end
+  
+end
